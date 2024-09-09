c13c3a575264c2a1102f7b31c0bbac54d832e9d8
dieizon cazuni <dieizon.cazuni.franco@gmail.com>
2024-09-09 17:33:42 -0300

    MEDIA

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!")
+/* seguindo a mesma estrutura do algoritmo de exemplo:
+escreva um algoritmo que calcula a média  aritmética do considerando as 3 notas do semestre */
+
+let nota1 = 9
+let nota2 = 8
+let nota3 = 10
+let soma = nota1 +nota2+nota3
+let media =soma/3
+console.log(media)
