042e27a20f1845741afe3b3f006aee5bc0e01149
DIEIZON CAZUNI FRANCO <dieizon.cazuni.franco@gmail.com>
2024-09-11 17:22:08 -0300

    aula 03

---
+++ dieizon.rs
@@ -0,0 +1,20 @@
+let array = ["futas", "melancia", "mamão"]
+
+// exercicio 01
+let arry = {"frutas","melancia","mamão","pera"}
+
+//exercio 02
+let arry  = ["melanncia", "mamão","pera"]
+// exercio 03
+let array = {"frutas", "melncia","mamão", "pera", "abacate",}
+
+//execicio 04 
+let array = {"frutas","melacia" ,}
+//xecicio 05 
+let array  =  {"goiaba","melancia", "mamão",
+   //execicio 06
+let array = {"frutas","melancia","mamão","pera", "abacate", "goiaba", "abacaxi",}
+    //exercicio 07
+let array =["frutas","melancia","mamão" "goiaba",]
+//execicio 08
+
